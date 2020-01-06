fc7a777cd4fed276b7cba5209d945acc400e522b
matsuirie <matsuirie@matsuirienoMacBook-Pro.local>
2020-01-06 23:35:09 +0900

    Refactor read_movie

--- src/read_movie.py
+++ src/read_movie.py
@@ -1,29 +1,43 @@
 import cv2
 
-# get information
 
-cap = cv2.VideoCapture("../mov/mov01.avi")
-num = 0
-width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-fps = cap.get(cv2.CAP_PROP_FPS)
-print("image width : " + str(width))
-print("image height : " + str(height))
-print("total frame count : " + str(count))
-print("FPS : " + str(fps))
+class ReadMovie(object):
 
-# show movie
-while cap.isOpened():
-    ret, frame = cap.read()
-    if ret:
-        cv2.imshow("frame", frame)
-        # divide avi
-        filePath = "../snapshot/snapshot_" + str(num) + ".jpeg"
-        cv2.imwrite(filePath, frame)
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            print(str(num) + ": image was output.")
-            break
-    num = num + 1
-cap.release()
-cv2.destroyAllWindows()
+    def __init__(self, filename):
+        capture = cv2.VideoCapture(filename)
+        self.capture = capture
+        self.width = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.height = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        self.count = capture.get(cv2.CAP_PROP_FRAME_COUNT)
+        self.frame_per_second = capture.get(cv2.CAP_PROP_FPS)
+
+    def print_value(self):
+        print("image width : " + str(self.width))
+        print("image height : " + str(self.height))
+        print("total frame count : " + str(self.count))
+        print("FPS : " + str(self.frame_per_second))
+        number = 0
+
+    def show_movie(self):
+        number = 0
+        # show movie
+        while self.capture.isOpened():
+            ret, frame = self.capture.read()
+            if ret:
+                cv2.imshow("frame", frame)
+                # divide avi
+                file_path = "../snapshot/snapshot_" + str(number) + ".jpeg"
+                cv2.imwrite(file_path, frame)
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    print(str(number) + ": image was output.")
+                    break
+            number = number + 1
+        self.capture.release()
+        cv2.destroyAllWindows()
+
+
+class MainReadMove:
+    readMovie = ReadMovie("../mov/mov01.avi")
+    readMovie.print_value()
+    readMovie.show_movie()
+
